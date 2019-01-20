9cb6e8880276ba20f9f04efb97b1de532bcb7cae
leeroy <liroy3000@gmail.com>
2019-01-20 23:08:10 +0300

    Task3: long job!

---
+++ task03/task03.py
@@ -0,0 +1,31 @@
+"""
+Задача проекта Эйлера
+https://projecteuler.net/problem=3
+
+Простые делители числа 13195 - это 5, 7, 13 и 29.
+Каков самый большой делитель числа 600851475143, являющийся простым числом?
+"""
+
+import math
+integer = 600851475143
+
+def isprime(i):
+	limit = math.ceil(math.sqrt(i))
+	result = True
+	for j in range(2, limit):
+		if i % j == 0:
+			result = False
+			break
+	return result
+
+
+
+for i in range(2, integer):
+	if integer % i == 0:
+		delimeter = integer / i
+		if isprime(delimeter):
+			max_delimeter = delimeter
+			break
+
+
+print(max_delimeter)
